d47fa63df1bec3a59e06212c6f72ed159bae83e0
lizKimita <kimita.wanjiku@gmail.com>
2019-05-09 21:15:59 +0300

    adding boostrap3

--- gallery/settings.py
+++ gallery/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'images_gallery.apps.ImagesGalleryConfig',
+    'images_gallery',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
