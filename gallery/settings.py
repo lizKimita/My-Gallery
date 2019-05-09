434ab40c53c58c956c48e81ad771c8573c0f3079
lizKimita <kimita.wanjiku@gmail.com>
2019-05-09 20:37:11 +0300

    adding the first test to view the homa page

--- gallery/settings.py
+++ gallery/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'images_gallery.apps.ImagesGalleryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- images_gallery/views.py
+++ images_gallery/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse, Http404
 import datetime as dt
 
 # Create your views here.
+def all_images(request):
+    title = "gallery"
+    return render(request, 'images.html')
