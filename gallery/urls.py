599aeefe71bbc1f6d812b9613571c08eb88cd512
lizKimita <kimita.wanjiku@gmail.com>
2019-05-10 14:24:52 +0300

    adding the image, location and category models

--- gallery/urls.py
+++ gallery/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^images_gallery/',include('images_gallery.urls'))
+    url(r'^',include('images_gallery.urls'))
 ]
--- images_gallery/models.py
+++ images_gallery/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'images/')
+    image_name = models.CharField(max_length = 30)
+    image_description = models.TextField()
+
+
+class Location(models.Model):
+    location = models.CharField(max_length = 30)
+
+
+class Category(models.Model):
+    category = models.CharField(max_length = 30)
