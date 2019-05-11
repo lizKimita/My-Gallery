e23e09aade2095e37ef94645dbe1de244e482c14
lizKimita <kimita.wanjiku@gmail.com>
2019-05-11 09:11:40 +0300

    adding and installing bootstrap modals

--- gallery/settings.py
+++ gallery/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'images_gallery',
     'bootstrap3',
+    'bootstrap_modal_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
